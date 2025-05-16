f69c702f1cc6ed6b92f7248e44fd2e8f02d5e418
huyle <lehuuduchuy@gmail.com>
2025-05-16 17:09:04 +0700

    change tokenADmin

--- middlewares/admin/auth.middlewares.js
+++ middlewares/admin/auth.middlewares.js
@@ -7,7 +7,7 @@ module.exports.requireAuth = async (req, res, next) => {
     //   next();
     // }
     // Lấy token từ cookie trước
-    let token = req.cookies.token;
+    let token = req.cookies.tokenADmin;
 
     // Nếu vẫn không có token, từ chối truy cập
     if (!token) {
